84a4d6f0e8947a534a9213b24eabc28e1ca88d12
Syun1208 <phamlong12082001@gmail.com>
2022-09-10 16:44:13 +0700

    Edit file Dataloader.py

--- Datasets/Dataloader.py
+++ Datasets/Dataloader.py
@@ -2,13 +2,17 @@ import socket
 import cv2
 import numpy as np
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT = 54321
+s.connect(('127.0.0.1', PORT))
+
 
 class Map:
     def __init__(self, currentAngle, currentSpeed, sendBackAngle, sendBackSpeed):
         self.PORT = 54321
         self.currentAngle = currentAngle
         self.currentSpeed = currentSpeed
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s = s
         self.sendBackAngle = sendBackAngle
         self.sendBackSpeed = sendBackSpeed
 
@@ -16,7 +20,6 @@ class Map:
         self.s.close()
 
     def Connect(self):
-        self.s.connect(('127.0.0.1', self.PORT))
         message_getState = bytes("0", "utf-8")
         self.s.sendall(message_getState)
         state_date = self.s.recv(1000)
@@ -35,3 +38,32 @@ class Map:
             ), -1
         )
         return image, self.currentAngle, self.currentSpeed, self.sendBackSpeed, self.sendBackAngle
+#     # Import socket module
+# def Map():
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     PORT = 54321
+#     s.connect(('127.0.0.1', PORT))
+#     while True:
+#         message_getState = bytes("0", "utf-8")
+#         s.sendall(message_getState)
+#         state_date = s.recv(100)
+#
+#         try:
+#             current_speed, current_angle = state_date.decode(
+#                         "utf-8"
+#                     ).split(' ')
+#             except Exception as er:
+#                     print(er)
+#                     pass
+#
+#             message = bytes(f"1 {sendBack_angle} {sendBack_Speed}", "utf-8")
+#             s.sendall(message)
+#             data = s.recv(100000)
+#
+#             try:
+#                 image = cv2.imdecode(
+#                 np.frombuffer(
+#                             data,
+#                             np.uint8
+#                         ), -1
+#                     )
